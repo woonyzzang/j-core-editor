b56063033888fc2de4b1f5719ab961f990fbff6c
woonyzzang <seungwoonjjang@gmail.com>
2018-12-18 00:09:22 +0900

    [J Core Editor] * Accodian 코드 리팩토링

--- public/modules/Accordion/Accordion.js
+++ public/modules/Accordion/Accordion.js
@@ -2,35 +2,20 @@
  * [UPLEAT] UI Dev Team
  * @module Accordion
  * @description 아코디언
- * @author CHO YEON SEUNG
+ * @author Cho Yeon Seung
+ * @email irene@upleat.com
  * @creat 2018.11.15
  * @version v1.0.0
- *
- * @param {Object} | 옵션 (optional)
- * @param {String} | Object.selector: 아코디언틀 선택자 (default - '.ui_accordion_wrap')
- * @param {String} | Object.title: 아코디언 타이틀 클래스명 (default - 'ui_accordion_tit')
- * @param {String} | Object.content: 아코디언 콘텐츠 클래스명  (default - 'ui_accordion_cont')
- * @param {Boolean} | Object.sliding: 아코디언 슬라이딩 효과 유무 (default - false)
- * @param {String} | Object.easing: 아코디언 슬라이딩 효과  (default - 'swing')
- * @param {String} | Object.seconds: 아코디언 슬라이딩 속도  (default - '400')
- * @param {Boolean} | Object.multi: 아코디언 열고닫힘 멀티유무 (default - true)
+ * @licence CC
  *
  * @example
- *  // HTML 구조
- *  <html>
- *  <body>
- *      <ul class="accordion_wrap">
- <li>
- <a href="#" role="button">타이틀 영역</a>
- <div>콘텐츠 영역</div>
- </li>
- <li>
- <button type="button">타이틀 영역</button>
- <div>콘텐츠 영역</div>
- </li>
- </ul>
- *  </body>
- *  </html>
+ * // HTML 구조
+ * <ul class="accordion_wrap">
+ *     <li>
+ *         <a href="#" role="button">타이틀 영역</a>
+ *         <div>콘텐츠 영역</div>
+ *     </li>
+ * </ul>
  */
 
 ;(function(global, doc, core, $) {
@@ -38,12 +23,21 @@
 
     core.module.Accordion = function(app) {
         app.accordion = core.Class({
+            /**
+             * @param {Object} options - 옵션 (optional)
+             * @param {String} Object.selector: 아코디언틀 선택자 (default - '.ui_accordion_area')
+             * @param {String} Object.title: 아코디언 타이틀 클래스명 (default - 'accordion_tit')
+             * @param {String} Object.content: 아코디언 콘텐츠 클래스명  (default - 'accordion_cont')
+             * @param {Boolean} Object.sliding: 아코디언 슬라이딩 효과 유무 (default - false)
+             * @param {String} Object.easing: 아코디언 슬라이딩 효과  (default - 'swing')
+             * @param {String} Object.seconds: 아코디언 슬라이딩 속도  (default - '400')
+             * @param {Boolean} Object.multi: 아코디언 열고닫힘 멀티유무 (default - true)
+             */
             __constructor: function(options) {
-
                 var defaults = {
-                    selector: '.ui_accordion_wrap', // 아코디언 틀 선택자
-                    title: 'ui_accordion_tit', // 아코디언 타이틀 클래스명
-                    content: 'ui_accordion_cont', //아코디언 콘텐츠 클래스명
+                    selector: '.ui_accordion_area', // 아코디언 틀 선택자
+                    title: 'accordion_tit', // 아코디언 타이틀 클래스명
+                    content: 'accordion_cont', //아코디언 콘텐츠 클래스명
                     sliding: false, // 아코디언 슬라이딩 효과 유무
                     easing: 'swing', // 아코디언 슬라이딩 효과
                     seconds: 400, // 아코디언 슬라이딩 속도
@@ -53,90 +47,102 @@
                 options = _.extend(defaults, options);
 
                 var $accordionList = $(options.selector).children();
+
                 $accordionList.children().addClass(options.content);
 
                 if ($accordionList.children('a, button')) { $accordionList.children('a, button').removeClass(options.content).addClass(options.title); }
 
                 this.$accordionWrap = $(options.selector);
                 this.$tit = this.$accordionWrap.find('.' + options.title);
-                this.$titActive = $('.' + options.title + '.active');
                 this.$cont = $('.' + options.content);
                 this.$sliding = options.sliding;
                 this.$easing = options.easing;
                 this.$sec = options.seconds;
+
                 this.init();
                 this.evtListener(options);
             },
 
-            // 초기화
-            init: function() {
-                this.$tit.attr({"title":"비활성화", "role":"heading", "aria-expanded":false}); // 접근성관련 타이틀 헤딩태그
-                this.$cont.attr({"tabindex":"-1", "role":"region", "aria-hidden":true}); // 접근성관련 콘텐츠 헤딩태그
-            },
-
-            // 접기
+            /**
+             * @method panelCollapse
+             * @description 패널 접기
+             * @param {Object} elem - DOM셀렉터
+             * @param {Object} options - 옵션
+             */
             panelCollapse: function(elem, options) {
                 var $elem = $(elem);
+
                 if (!this.$sliding) {
-                    $elem.attr({"title":"비활성화", "aria-expanded":false}).removeClass('active').next(this.$cont).attr({"tabindex":"-1", "aria-hidden":true}).hide();
-                    if (options === 'all') {  // 전체접기(hide)
+                    $elem.attr({title: '비활성화', 'aria-expanded': false}).removeClass('active').next(this.$cont).attr({tabindex: -1, 'aria-hidden': true}).hide();
+                    if (options === 'all') { // 전체접기(hide)
                         this.$tit.each(function() {
-                            $.$(this).attr({"title":"비활성화", "aria-expanded":false}).removeClass('active').next(this.$cont).attr({"tabindex":"-1", "aria-hidden":true}).hide();
+                            $.$(this).attr({title: '비활성화', 'aria-expanded': false}).removeClass('active').next(this.$cont).attr({tabindex: -1, 'aria-hidden': true}).hide();
                         });
                     }
                 } else {
-                    $elem.attr({"title":"비활성화", "aria-expanded":false}).removeClass('active').next(this.$cont).attr({"tabindex":"-1", "aria-hidden":true}).slideUp(this.$sec, this.$easing);
+                    $elem.attr({title: '비활성화', 'aria-expanded': false}).removeClass('active').next(this.$cont).attr({tabindex: -1, 'aria-hidden': true}).slideUp(this.$sec, this.$easing);
+
                     if (options === 'all') { // 전체접기(sliding)
                         this.$tit.each(function() {
-                            $.$(this).attr({"title":"비활성화", "aria-expanded":false}).removeClass('active').next(this.$cont).attr({"tabindex":"-1", "aria-hidden":true}).slideUp(this.$sec, this.$easing);
+                            $.$(this).attr({title: '비활성화', 'aria-expanded': false}).removeClass('active').next(this.$cont).attr({tabindex: -1, 'aria-hidden': true}).slideUp(this.$sec, this.$easing);
                         });
                     }
                 }
             },
 
-            // 펼치기
+            /**
+             * @method panelCollapse
+             * @description 패널 펼치기
+             * @param {Object} elem - DOM셀렉터
+             * @param {Object} options - 옵션
+             */
             panelExpansion: function(elem, options) {
                 var $elem = $(elem);
+                
                 if (!this.$sliding) {
                     if (options.multi) { // 한 패널 선택시 다른 패널은 자동으로 닫힘
-                        $elem.attr({"title":"활성화", "aria-expanded":true}).addClass('active').next(this.$cont).attr({"tabindex":"0", "aria-hidden":false}).show();
-                        $elem.parent().siblings().children('a, button').attr({"title":"비활성화", "aria-expanded":false}).removeClass('active').next(this.$cont).attr({"tabindex":"-1","aria-hidden":true}).hide();
+                        $elem.attr({title: '활성화', 'aria-expanded': true}).addClass('active').next(this.$cont).attr({tabindex: 0, 'aria-hidden': false}).show();
+                        $elem.parent().siblings().children('a, button').attr({title: '비활성화', 'aria-expanded': false}).removeClass('active').next(this.$cont).attr({'tabindex': -1, 'aria-hidden': true}).hide();
                     } else { // 한 패널 선택시 다른 패널 그대로 열려있음
-                        $elem.attr({"title":"활성화", "aria-expanded":true}).addClass('active').next(this.$cont).attr({"tabindex":"0", "aria-hidden":false}).show();
+                        $elem.attr({title: '활성화', 'aria-expanded': true}).addClass('active').next(this.$cont).attr({tabindex: 0, 'aria-hidden': false}).show();
                     }
 
                     if (options === 'all') { // 전체펼치기(hide)
                         this.$tit.each(function() {
-                            $.$(this).attr({"title":"활성화", "aria-expanded":true}).addClass('active').next(this.$cont).attr({"tabindex":"0", "aria-hidden":false}).show();
+                            $.$(this).attr({title: '활성화', 'aria-expanded': true}).addClass('active').next(this.$cont).attr({tabindex: 0, 'aria-hidden': false}).show();
                         });
                     }
                 } else {
                     if (options.multi) { // 한 패널 선택시 다른 패널은 자동으로 닫힘
-                        $elem.attr({"title":"활성화", "aria-expanded":true}).addClass('active').next(this.$cont).attr({"tabindex":"0", "aria-hidden":false}).slideDown(this.$sec, this.$easing);
-                        $elem.parent().siblings().children('a, button').attr({"title":"비활성화", "aria-expanded":false}).removeClass('active').next(this.$cont).attr({"tabindex":"-1", "aria-hidden":true}).slideUp(this.$sec, this.$easing);
+                        $elem.attr({title: '활성화', 'aria-expanded': true}).addClass('active').next(this.$cont).attr({tabindex: 0, 'aria-hidden': false}).slideDown(this.$sec, this.$easing);
+                        $elem.parent().siblings().children('a, button').attr({title: '비활성화', 'aria-expanded': false}).removeClass('active').next(this.$cont).attr({tabindex: -1, 'aria-hidden': true}).slideUp(this.$sec, this.$easing);
                     } else { // 한 패널 선택시 다른 패널 그대로 열려있음
-                        $elem.attr({"title":"활성화", "aria-expanded":true}).addClass('active').next(this.$cont).attr({"tabindex":"0", "aria-hidden":false}).slideDown(this.$sec, this.$easing);
+                        $elem.attr({title: '활성화', 'aria-expanded': true}).addClass('active').next(this.$cont).attr({tabindex: 0, 'aria-hidden': false}).slideDown(this.$sec, this.$easing);
                     }
 
                     if (options === 'all') { // 전체펼치기(sliding)
                         this.$tit.each(function() {
-                            $.$(this).attr({"title":"활성화", "aria-expanded":true}).addClass('active').next(this.$cont).attr({"tabindex":"0", "aria-hidden":false}).slideDown(this.$sec, this.$easing);
+                            $.$(this).attr({title: '활성화', 'aria-expanded': true}).addClass('active').next(this.$cont).attr({tabindex: 0, 'aria-hidden': false}).slideDown(this.$sec, this.$easing);
                         });
                     }
                 }
             },
 
-            // 이벤트
+            /** 초기화 */
+            init: function() {
+                this.$tit.attr({title: '비활성화', role: 'heading', 'aria-expanded': false}); // 접근성관련 타이틀 헤딩태그
+                this.$cont.attr({tabindex: -1, role: 'region', 'aria-hidden': true}); // 접근성관련 콘텐츠 헤딩태그
+            },
+
+            /** 이벤트 핸들러 */
             evtListener: function(options) {
                 var _this = this;
+
                 _this.$tit.on('click', function(e) {
                     e.preventDefault();
-                    if(!$(this).next(this.$cont).is(':animated')){
-                        if (!$(this).is('.active')) {
-                            _this.panelExpansion(this, options);
-                        } else {
-                            _this.panelCollapse(this, options);
-                        }
+
+                    if (!$(this).next(this.$cont).is(':animated')) {
+                        (!$(this).is('.active'))? _this.panelExpansion(this, options) : _this.panelCollapse(this, options);
                     }
                 });
             }
